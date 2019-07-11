30e6f585d24c244dc789c3ce15236f766b02f930
tfsui3 <46233292+tfsui3@users.noreply.github.com>
2019-07-10 23:29:21 -0500

    Add files via upload

---
+++ vector practice.cpp
@@ -0,0 +1,20 @@
+//
+//  vector practice.cpp
+//  xcode_test1
+//
+//  Created by tfsui3 on 2019/7/10.
+//  Copyright © 2019 tfsui3. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cin>>i;
+    cout<<i;
+    return 0;
+}
